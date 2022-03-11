81f744288e9163ff84b98e0166ee984e57bde78a
JawaraGordon <jawaragordon@icloud.com>
2022-03-11 16:43:28 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,48 @@
+function shout(string){
+    return string.toUpperCase();
+}
+console.log(shout("hello"));
+
+
+function whisper(string){
+    return string.toLowerCase();
+}
+
+function logShout(string){
+    console.log(string.toUpperCase());
+}
+function logWhisper(string){
+    console.log(string.toLowerCase());
+}
+
+// console.log(Shout('hello'));
+
+
+// Define Variables 
+
+
+// Declare Function 
+function sayHiToHeadphonedRoommate(string){
+
+const tooSoft = "I can't hear you!"
+const tooLoud = "YES INDEED!"
+const justRight = "I would love to!"
+
+    if (string === string.toLowerCase()){
+        return tooSoft;
+    }
+    else if (string === string.toUpperCase()){
+        return tooLoud;
+    }
+    else if 
+    (string === "Let\'s have dinner together!"){
+        return justRight;
+    }
+        
+}
+
+sayHiToHeadphonedRoommate("Let's have dinner together!");
+
+// If..logic
+
+//Call Func
